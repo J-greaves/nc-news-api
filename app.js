e804f27f2f2eec5a3b134266feeaed0fc5a2b333
J-greaves <jacksongreaves@googlemail.com>
2024-08-23 17:09:30 +0100

    removed a peice of error middleware, related test and a console.log as per feedback

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ const app = express();
 
 const { getTopics } = require("./controllers/topics-controllers");
 
-app.use(express.json());
-
-app.all("/api/topics", (req, res, next) => {
-  if (req.method !== "GET") {
-    res.status(405).send({ msg: "Method not allowed" });
-  } else {
-    next();
-  }
-});
-
 app.get("/api/topics", getTopics);
 
 app.use((req, res, next) => {
