deb83124932b9c7b3b40c4793fa32af52b775ad2
J-greaves <jacksongreaves@googlemail.com>
2024-08-27 15:48:13 +0100

    completed task 5, couldn't think of any errors to test for with this endpoint

--- models/articles-models.js
+++ models/articles-models.js
@@ -1,11 +1,6 @@
 const db = require("../db/connection");
 
 exports.fetchArticleById = (article_id) => {
-  if (isNaN(article_id)) {
-    return Promise.reject({
-      msg: "Invalid article id",
-    });
-  }
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
     .then((result) => {
@@ -15,5 +10,28 @@ exports.fetchArticleById = (article_id) => {
         });
       }
       return result.rows[0];
+    });
+};
+
+exports.fetchArticles = () => {
+  return db
+    .query(
+      `
+        SELECT articles.article_id,
+        articles.title,
+        articles.author,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        COUNT(comments.comment_id) AS comment_count
+        FROM articles
+        LEFT JOIN comments ON articles.article_id = comments.article_id
+        GROUP BY articles.article_id
+        ORDER BY articles.created_at DESC
+    `
+    )
+    .then((result) => {
+      return result.rows;
     });
 };
